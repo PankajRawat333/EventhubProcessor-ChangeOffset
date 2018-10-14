ab63f3012f871637a9284795fd94ac6425cc0f8a
Pankaj Rawat <pankajrawat.333@hotmail.com>
2018-10-14 23:13:06 +0530

    Add project files.

---
+++ EventHubProcessorNew1/Program.cs
@@ -0,0 +1,39 @@
+﻿using EventHubProcessor1;
+using Microsoft.ServiceBus.Messaging;
+using System;
+
+namespace EventHubProcessorNew1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=**************.servicebus.windows.net/;SharedAccessKeyName=AlertingListen;SharedAccessKey==**************.";
+            string eventHubName = "prd-=**************.-eh-01";
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=prdamsussccatsa01;AccountKey==**************;EndpointSuffix=core.windows.net";
+
+            string _guid = Guid.NewGuid().ToString();
+            string eventProcessorHostName = _guid;
+
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(
+                eventProcessorHostName,
+                eventHubName,
+                "alerting",
+                eventHubConnectionString,
+                storageConnectionString, "local-webjobs-eventhub"
+                );
+
+
+            Console.WriteLine("Registering EventProcessor...!");
+
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(new EventProcessorOptions
+            {
+                InitialOffsetProvider = (partitionId) => DateTime.UtcNow
+            }).Wait();
+
+            Console.WriteLine("Press enter to stop");
+            Console.Read();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
---
+++ EventHubProcessorNew1/SimpleEventProcessor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventHubProcessor1
+{
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine($"Processor Shutting Down.");
+            return Task.WhenAll();
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"SimpleEventProcessor initialized.");
+            return Task.WhenAll();
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine($" Error: {error.Message}");
+            return Task.WhenAll();
+        }
+
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                Console.WriteLine($"Message received.");
+            }
+
+            return context.CheckpointAsync();
+        }
+    }
+}
